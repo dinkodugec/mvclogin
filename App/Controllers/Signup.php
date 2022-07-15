8d6d73a0c079eafabf0785e4363d9e8ea8c060a3
Dinko <dinko.dugec@gmail.com>
2022-07-15 22:32:25 +0200

    Create and display the signup page

---
+++ App/Controllers/Signup.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controllers;
+
+use \Core\View;
+
+/**
+ * Signup controller
+ *
+ * 
+ */
+class Signup extends \Core\Controller
+{
+  /**
+   * Show the signup page
+   *
+   * @return void
+   */
+  public function newAction()
+  {
+      View::renderTemplate('Signup/new.html');
+  }
+}
