0261f3be086abdc60fee0b0c29fb752141c5f196
Dinko <dinko.dugec@gmail.com>
2022-08-08 19:54:56 +0200

    process the forgotten password form in the controller

--- App/Controllers/Password.php
+++ App/Controllers/Password.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use \Core\View;
+use \App\Models\User;
 
 /**
  * Password controller
@@ -19,5 +20,17 @@ class Password extends \Core\Controller
     public function forgotAction()
     {
         View::renderTemplate('Password/forgot.html');
+    }
+
+    /**
+     * Send the password reset link to the supplied email
+     *
+     * @return void
+     */
+    public function requestResetAction()
+    {
+        User::sendPasswordReset($_POST['email']); /* passing email from form */
+
+        View::renderTemplate('Password/reset_requested.html');
     }
 }
