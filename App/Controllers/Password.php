204b0f328f0a60c80ffdde39e62134ee2a4a8582
Dinko <dinko.dugec@gmail.com>
2022-08-12 21:20:02 +0200

    get user based on the token and check the expiry

--- App/Controllers/Password.php
+++ App/Controllers/Password.php
@@ -44,5 +44,9 @@ class Password extends \Core\Controller
         $token = $this->route_params['token'];
 
         echo $token;
+
+        $user = User::findByPasswordReset($token); //passing token from url
+
+        var_dump($user);
     }
 }
